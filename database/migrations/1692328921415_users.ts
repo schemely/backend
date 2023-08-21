f6b4fd2aa1b5a4140bb1fa2060928cf5260bac0a
b01010110 <79957072+b01010110@users.noreply.github.com>
2023-08-21 16:15:58 +0700

    Addition Auth
    - db connection has been installed
    - auth has been installed
    - auth has been added

---
+++ database/migrations/1692328921415_users.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('email', 255).notNullable().unique()
+      table.string('password', 180)
+      table.string('avatar_url')
+      table.integer('github_id')
+      table.string('github_login', 50)
+      table.string('github_code', 64)
+      table.string('github_token', 64)
+      table.string('remember_me_token').nullable()
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
